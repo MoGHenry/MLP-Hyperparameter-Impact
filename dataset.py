6c4352147f79814c03e9b9b122702d1840ba8207
MoG Henry <84988222+MoGHenry@users.noreply.github.com>
2024-10-09 01:40:38 -0400

    implement functions
    not done, not tested yet

--- dataset.py
+++ dataset.py
@@ -1,18 +1,48 @@
 import pandas as pd
 import numpy as np
-from scipy import ndimage
+
 
 # https://youtu.be/w8yWXqWQYmU?si=MOIwj0waNpEunSJS&t=709
 
+
 # load the data from csv file as pd.DataFrame
-def load_data(path):
-    return
+def load_data(path: str) -> pd.DataFrame:
+    # read the csv file
+    data = pd.read_csv(path)
+    return data
+
 
 # convert to numpy array
-def preprocess_data(data):
-    return
+def preprocess_data(data: pd.DataFrame) -> np.ndarray:
+    # convert to numpy array
+    data = np.array(data)
+    return data
+
 
 # split the data into training and testing sets
-def split_data(data):
-    return
+# first col is the label, rest are features
+def split_data(data: np.ndarray, size: float = 0.8) -> dict:
+    # Shuffle the data
+    np.random.shuffle(data)
 
+    # Determine the index for splitting the data
+    split_index = int(size * data.shape[0])
+
+    # Split the data into training and testing sets
+    train_data = data[:split_index, :].T
+    test_data = data[split_index:, :].T
+
+    # The first row contains the labels, and the rest are features
+    train_labels = train_data[0].reshape(1, -1)
+    validation_labels = test_data[0].reshape(1, -1)
+    train_features = train_data[1:]
+    validation_features = test_data[1:]
+
+    training_data = {
+        "train_features": train_features,
+        "train_labels": train_labels,
+        "test_features": validation_features,
+        "test_labels": validation_labels
+    }
+
+    return training_data
--- neural_network.py
+++ neural_network.py
@@ -8,64 +8,160 @@ import numpy as np
 # b1 shape (1, hidden_size)
 # w2 shape (hidden_size, output_size)
 # b2 shape (1, output_size)
-def initialize_parameters(input_size, hidden_size, output_size):
-    return
+def std_initialize_parameters(input_size, hidden_size, output_size):
+    w1 = np.random.randn(hidden_size, input_size)
+    b1 = np.zeros((hidden_size, 1))
+    w2 = np.random.randn(output_size, hidden_size)
+    b2 = np.zeros((output_size, 1))
+    parameters = {
+        "w1": w1,
+        "b1": b1,
+        "w2": w2,
+        "b2": b2
+    }
+    return parameters
+
+
+def he_initialize_parameters(input_size, hidden_size, output_size):
+    w1 = np.random.randn(hidden_size, input_size) * np.sqrt(2. / input_size)
+    b1 = np.zeros((hidden_size, 1))
+    w2 = np.random.randn(output_size, hidden_size) * np.sqrt(2. / hidden_size)
+    b2 = np.zeros((output_size, 1))
+    parameters = {
+        "w1": w1,
+        "b1": b1,
+        "w2": w2,
+        "b2": b2
+    }
+    return parameters
 
 
 # forward propagation
 # calculate z1, a1, z2, a2
 # return
-def forward_propagation(x, w1, b1, w2, b2):
-    return
+def forward_propagation(x, w1, b1, w2, b2, activation_function: str):
+    activation_functions = {
+        "relu": activation_function_relu,
+        "sigmoid": activation_function_sigmoid,
+        "tanh": activation_function_tanh
+    }
+    z1 = np.dot(w1, x) + b1
+    a1 = activation_functions[activation_function](z1)
+    z2 = np.dot(w2, a1) + b2
+    a2 = softmax(z2)
+    forward_cache = {
+        "z1": z1,
+        "a1": a1,
+        "z2": z2,
+        "a2": a2
+    }
+    return forward_cache
 
 
 # activation function
 # relu, sigmoid, tanh, softmax
-def activation_function_xxx(z):
-    return
+def activation_function_relu(z):
+    return np.maximum(0, z)
+
+
+def activation_function_sigmoid(z):
+    return 1. / (1. + np.exp(-z))
+
+
+def activation_function_tanh(z):
+    return np.tanh(z)
 
 
 # derivation of activation function
 # relu, sigmoid, tanh, softmax
-def activation_function_derivative_xxx(z):
-    return
+def activation_function_derivative_relu(z):
+    return np.greater(z, 0).astype(int)
+
+
+def activation_function_derivative_sigmoid(z):
+    return activation_function_sigmoid(z) * (1. - activation_function_sigmoid(z))
+
+
+def activation_function_derivative_tanh(z):
+    return 1. - np.power(activation_function_tanh(z), 2)
 
 
 # softmax function
 def softmax(z):
-    return
+    return np.exp(z) / np.sum(np.exp(z), axis=0)
 
 
 # one-hot encoding
-def one_hot(y):
-    return
+# num_classes is the number of unique labels in the output
+def one_hot(y, num_classes):
+    return np.eye(num_classes)[y.reshape(-1)]
 
 
 # backward propagation
 # calculate dw1, db1, dw2, db2
-def backward_propagation(x, y, z1, a1, z2, a2, w2):
-    return
+def backward_propagation(x, y, z1, a1, z2, a2, w2, derivative_activation_function: str):
+    one_hot_y = one_hot(y, a2.shape[0])
+    dz2 = a2 - one_hot_y
+    dw2 = np.dot(dz2, a1.T)
+    db2 = np.sum(dz2, axis=1, keepdims=True)
+    da1 = np.dot(w2.T, dz2)
+    dz1 = da1 * activation_function_derivative_relu(z1)
+    dw1 = np.dot(dz1, x.T)
+    db1 = np.sum(dz1, axis=1, keepdims=True)
+    backward_cache = {
+        "dw1": dw1,
+        "db1": db1,
+        "dw2": dw2,
+        "db2": db2
+    }
+    return backward_cache
 
 
 def update_parameters(w1, b1, w2, b2, dw1, db1, dw2, db2, learning_rate):
-    return
+    updated_parameters = {
+        "w1": w1 - learning_rate * dw1,
+        "b1": b1 - learning_rate * db1,
+        "w2": w2 - learning_rate * dw2,
+        "b2": b2 - learning_rate * db2
+    }
+    return updated_parameters
 
 
 def get_predictions(a2):
-    return
+    return np.argmax(a2, axis=0)
 
 
 def get_accuracy(y_pred, y_true):
+    return np.mean(y_pred == y_true)
+
 
 # in each iteration
 # forward propagation
 # backward propagation
 # update parameters
 # print the accuracy and loss
-def gradient_descent(X, y, w1, b1, w2, b2, learning_rate, num_iterations):
-    return
+# TODO change parameters to dictionary
+def gradient_descent(X, y, w1, b1, w2, b2, learning_rate, num_iterations, activation_function
+                     , derivative_activation_function="relu"):
+    for i in range(num_iterations):
+        forward_cache = forward_propagation(X, w1, b1, w2, b2, activation_function)
+        backward_cache = backward_propagation(X, y, forward_cache["z1"], forward_cache["a1"],
+                                              forward_cache["z2"], forward_cache["a2"], w2,
+                                              derivative_activation_function)
+        updated_parameters = update_parameters(w1, b1, w2, b2, backward_cache["dw1"], backward_cache["db1"],
+                                               backward_cache["dw2"], backward_cache["db2"], learning_rate)
+        w1 = updated_parameters["w1"]
+        b1 = updated_parameters["b1"]
+        w2 = updated_parameters["w2"]
+        b2 = updated_parameters["b2"]
+        if i % 100 == 0:
+            y_pred = get_predictions(forward_cache["a2"])
+            accuracy = get_accuracy(y_pred, y)
+            print(f"Iteration {i}, accuracy: {accuracy}")
+    return w1, b1, w2, b2
 
 
+# TODO
 def make_prediction(X, w1, b1, w2, b2):
     return
 
