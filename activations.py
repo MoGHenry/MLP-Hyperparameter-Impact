dc142ca3970e74ca7f627592fdf261461b439db9
MoG Henry <84988222+MoGHenry@users.noreply.github.com>
2024-11-18 23:30:49 -0500

    fix activation assign bug

--- activations.py
+++ activations.py
@@ -1,17 +1,6 @@
 import numpy as np
 
 
-def get_activation_functions(self, activation="relu"):
-    if activation == "relu":
-        return activation_function_relu, activation_function_derivative_relu
-    elif activation == "sigmoid":
-        return activation_function_sigmoid, activation_function_derivative_sigmoid
-    elif activation == "tanh":
-        return activation_function_tanh, activation_function_derivative_tanh
-    else:
-        raise ValueError("Invalid activation function")
-
-
 # activation function
 # relu, sigmoid, tanh, softmax
 def activation_function_relu(z):
@@ -39,3 +28,14 @@ def activation_function_derivative_sigmoid(z):
 
 def activation_function_derivative_tanh(z):
     return 1. - np.power(activation_function_tanh(z), 2)
+
+
+def get_activation_functions(activation="relu"):
+    if activation == "relu":
+        return activation_function_relu, activation_function_derivative_relu
+    elif activation == "sigmoid":
+        return activation_function_sigmoid, activation_function_derivative_sigmoid
+    elif activation == "tanh":
+        return activation_function_tanh, activation_function_derivative_tanh
+    else:
+        raise ValueError("Invalid activation function")
